7bc1ad5f56c0077b65cd9f12dea95e3a7d71f450
Rhesa Febrin Saputra <rhesa.saputra@gmail.com>
2018-07-04 16:09:41 +0700

    PLER

--- challengeGit/TestViewController.swift
+++ challengeGit/TestViewController.swift
@@ -12,7 +12,7 @@ class TestViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+//MEMEK
         // Do any additional setup after loading the view.
     }
 
